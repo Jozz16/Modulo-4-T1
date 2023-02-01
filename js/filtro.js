5c3dff3b679c5efd47273adc164b61dc63d6ed2a
José <jo633554@gmail.com>
2023-02-01 13:02:32 -0300

    cambio

--- js/filtro.js
+++ js/filtro.js
@@ -3,6 +3,8 @@ const div = document.getElementById("pacientes");
 
 
 paciente.datosPaciente.forEach(elemento=>{
+
+
     const persona = new Paciente(elemento.nombre, elemento.rut, elemento.edad, elemento.diagnostico); 
     console.log(persona)
     div.innerHTML +=  persona.nombrar();
